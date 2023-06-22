aa5155c73689d1ff267b22b27a440f55b4283256
Adrian Twardawski <twardawski@vialutions.com>
2023-06-22 10:14:18 +0200

    migration with cascade delete

--- Entities/MyBoardsContext.cs
+++ Entities/MyBoardsContext.cs
@@ -85,6 +85,10 @@ namespace MyBoards2.Entities
             {
                 eb.Property(x => x.CreatedDate).HasDefaultValueSql("getutcdate()");
                 eb.Property(x => x.UpdatedDate).ValueGeneratedOnUpdate();
+                eb.HasOne(c => c.Author)
+                .WithMany(a => a.Comments)
+                .HasForeignKey(c => c.AuthorId)
+                .OnDelete(DeleteBehavior.NoAction);
             });
 
             modelBuilder.Entity<User>()
