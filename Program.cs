f492d1e9095b4d8174d349cb3ef722b8aa130263
Adrian Twardawski <twardawski@vialutions.com>
2023-06-22 09:16:22 +0200

    custom seed logic

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using MyBoards2.Entities;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -25,6 +25,36 @@ var pendingMigrations = dbContext.Database.GetPendingMigrations();
 if (pendingMigrations.Any())
 {
     dbContext.Database.Migrate();
+}
+
+var users = dbContext.Users.ToList();
+if (!users.Any())
+{
+    var user1 = new User()
+    {
+        Email = "user1@test.com",
+        FullName = "User One",
+        Address = new Address()
+        {
+            City = "Warszawa",
+            Street = "Szeroka",
+        }
+    };
+
+    var user2 = new User()
+    {
+        Email = "user2@test.com",
+        FullName = "User Two",
+        Address = new Address()
+        {
+            City = "Kraków",
+            Street = "Długa",
+        }
+    };
+
+    dbContext.Users.AddRange(user1, user2);
+
+    dbContext.SaveChanges();
 }
 
 app.Run();
