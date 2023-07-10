22f7f762ae80da1d5ae61d9b24a479b671a1a232
Adrian Twardawski <twardawski@vialutions.com>
2023-07-10 13:46:17 +0200

    abuse of include

--- Northwind/Program.cs
+++ Northwind/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using Northwind.Entities;
+using System.Linq.Expressions;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -34,31 +35,37 @@ app.MapGet("data", async (NorthwindContext db) =>
 
 app.MapGet("getOrderDetails", async (NorthwindContext db) =>
 {
-    Order order = await GetOrder(46, db);
+    Order order = await GetOrder(46, db, o => o.OrderDetails);
     return new { OrderId = order.OrderId, Details = order.OrderDetails };
 });
 
 app.MapGet("getOrderWithShipper", async (NorthwindContext db) =>
 {
-    Order order = await GetOrder(46, db);
+    Order order = await GetOrder(46, db, o => o.ShipViaNavigation);
     return new { OrderId = order.OrderId, ShipVia = order.ShipVia, Shipper = order.ShipViaNavigation };
 });
 
 app.MapGet("getOrderWithCustomer", async (NorthwindContext db) =>
 {
-    Order order = await GetOrder(46, db);
-    return new { OrderId = order.OrderId, Customer = order.Customer };
+    Order order = await GetOrder(46, db, o => o.Customer, o => o.Employee);
+    return new { OrderId = order.OrderId, Customer = order.Customer, Employee = order.Employee};
 });
 
 app.Run();
 
-async Task<Order> GetOrder(int orderId, NorthwindContext db)
+async Task<Order> GetOrder(int orderId, NorthwindContext db, params Expression<Func<Order, object>>[] includes)
 {
-    Order order = await db.Orders
-        .Include(o => o.OrderDetails)
-        .Include(o => o.ShipViaNavigation)
-        .Include(o => o.Customer)
-        .FirstAsync(o => o.OrderId == orderId);
+    var baseQuery = db.Orders
+        .AsQueryable(); ;
+    if (includes.Any())
+    {
+        foreach (var include in includes)
+        {
+            baseQuery = baseQuery.Include(include);
+        }
+    }
+
+    var order = await baseQuery.FirstAsync(o => o.OrderId == orderId);
 
     return order; 
 }
