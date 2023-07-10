97caba08f941e91d7e92ba77598e37c06462d6d1
Adrian Twardawski <twardawski@vialutions.com>
2023-07-10 13:38:06 +0200

    example queires

--- Northwind/Program.cs
+++ Northwind/Program.cs
@@ -25,10 +25,41 @@ app.UseHttpsRedirection();
 
 app.MapGet("data", async (NorthwindContext db) =>
 {
-    var sampleData = await db.Products.Take(100).ToListAsync();
+    var sampleData = await db.Products
+        .Take(100)
+        .ToListAsync();
 
     return sampleData;
 });
 
+app.MapGet("getOrderDetails", async (NorthwindContext db) =>
+{
+    Order order = await GetOrder(46, db);
+    return new { OrderId = order.OrderId, Details = order.OrderDetails };
+});
+
+app.MapGet("getOrderWithShipper", async (NorthwindContext db) =>
+{
+    Order order = await GetOrder(46, db);
+    return new { OrderId = order.OrderId, ShipVia = order.ShipVia, Shipper = order.ShipViaNavigation };
+});
+
+app.MapGet("getOrderWithCustomer", async (NorthwindContext db) =>
+{
+    Order order = await GetOrder(46, db);
+    return new { OrderId = order.OrderId, Customer = order.Customer };
+});
+
 app.Run();
+
+async Task<Order> GetOrder(int orderId, NorthwindContext db)
+{
+    Order order = await db.Orders
+        .Include(o => o.OrderDetails)
+        .Include(o => o.ShipViaNavigation)
+        .Include(o => o.Customer)
+        .FirstAsync(o => o.OrderId == orderId);
+
+    return order; 
+}
 
